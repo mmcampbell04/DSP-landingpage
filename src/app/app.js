9d65ba63ee5d921c60fe103927caedd7cbd95565
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2021-12-19 22:43:59 +0000

    styling and logic for dark/light modes

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,42 @@
-console.log("boooooo!");
+const toggle = document.getElementById("toggle");
+const checkSystemPreferences = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+);
+
+const setDarkMode = () => {
+  document.body.classList.remove("light-theme");
+  document.body.classList.add("dark-theme");
+  localStorage.setItem("colorMode", "dark");
+};
+
+const setLightMode = () => {
+  document.body.classList.remove("dark-theme");
+  document.body.classList.add("light-theme");
+  localStorage.setItem("colorMode", "light");
+};
+
+// checks and uses system's theme, activates checkbox;
+const setColorScheme = (e) => {
+  if (e.matches) {
+    // Dark
+    toggle.checked = true;
+    console.log("dark mode");
+  } else {
+    // Light
+    toggle.checked = false;
+    console.log("light mode");
+  }
+};
+
+setColorScheme(checkSystemPreferences);
+checkSystemPreferences.addEventListener("change", setColorScheme);
+
+// toggles theme based on user changes
+toggle.addEventListener("input", (e) => {
+  const isChecked = e.target.checked;
+  if (isChecked) {
+    setDarkMode();
+  } else {
+    setLightMode();
+  }
+});
