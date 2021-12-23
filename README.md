ff134c7bd50b73da75fda602b908eaab5a537fb3
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2021-12-23 17:01:32 +0000

    styles & logic for mobile navbar

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 Landing page for "Dead Squirrel Productions", a quasi-serious business.
 
-Design inspiration courtesy of Jhey Thompkins.
+Design inspiration courtesy of Jhey Thompkins and various images of squirrels.
+
+Copy written by me.
 
 What I've learned so far:
 
 - how to create my own dark mode toggle
 - how to adapt someone else's dark mode toggle (coder, coder);
-- how to debug my own original dark mode toglle
-- css animations (Jhey Thompkins, Codepen);
+- how to debug my own original dark mode toggle...
+- css animations/illustrations (Jhey Thompkins, Codepen);
--- src/app/app.js
+++ src/app/app.js
@@ -59,3 +59,22 @@ const checkAndSetTheme = () => {
 checkSystemPreferences.addEventListener("change", setColorScheme);
 
 checkAndSetTheme();
+
+// MOBILE NAV TOGGLE
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const nav = document.querySelector(".primary-navigation");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+const activateMobileMenu = () => {
+  hamburgerMenu.classList.toggle("animate");
+  const visibility = nav.getAttribute("data-visible");
+  if (visibility === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+};
+
+navToggle.addEventListener("click", activateMobileMenu);
