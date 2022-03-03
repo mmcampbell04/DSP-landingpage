be4c27b71464af8e0ec87994051440adc8e7b378
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-03-03 17:22:14 +0000

    removed js for scroll to for scroll margins

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -30,38 +30,44 @@ export function setFixedNav() {
   }
 
   // show the back to top button when scrolling past a certain point
-  if (scrollHeight > 500) {
+  if (scrollHeight > 600) {
     btnUp.classList.add("show-btn");
   } else {
     btnUp.classList.remove("show-btn");
   }
 }
 
-// scroll to specific section on link click
-export function getScrollHeight(e) {
-  // prevent default behaviour
-  e.preventDefault();
-  // scroll to specific spot based on the href and removing the #
-  const id = e.currentTarget.getAttribute("href").slice(1);
-  console.log(id);
-  const domElement = document.getElementById(id);
+// // scroll to specific section on link click
+// export function getScrollHeight(e) {
+//   // prevent default behaviour
+//   e.preventDefault();
+//   // scroll to specific spot based on the href and removing the #
+//   const id = e.currentTarget.getAttribute("href").slice(1);
+//   console.log(id);
+//   const domElement = document.getElementById(id);
+//   console.log(domElement);
 
-  const navHeight = navbar.getBoundingClientRect().height;
-  const fixedNav = navbar.classList.contains("fixed-nav");
-  //   if the navbar IS fixed, subtract the navheight as its fixed, so its taken out of the normal flow & height of the document
-  let position = domElement.offsetTop - navHeight;
+//   const navHeight = navbar.getBoundingClientRect().height;
+//   console.log(navHeight);
+//   const fixedNav = navbar.classList.contains("fixed-nav");
+//   //   if the navbar IS fixed, subtract the navheight as its fixed, so its taken out of the normal flow & height of the document
+//   let position = domElement.offsetTop - navHeight;
 
-  // if the nav is NOT fixed (i.e. user at the veryt op of the page)
-  if (!fixedNav) {
-    position = position - navHeight;
-  }
+//   // if the nav is NOT fixed (i.e. user at the veryt op of the page)
+//   if (!fixedNav) {
+//     position = position - navHeight;
+//   }
 
-  window.scrollTo({
-    left: 0,
-    top: position,
-  });
+//   window.scrollTo({
+//     left: 0,
+//     top: position,
+//   });
 
-  //   close mobile menu
+//   //   close mobile menu
+//   closeMobileNav();
+// }
+
+export function closeMobileNav() {
   navLinks.setAttribute("data-visible", false);
   navToggle.setAttribute("aria-expanded", false);
   hamburgerMenu.classList.remove("animate");
